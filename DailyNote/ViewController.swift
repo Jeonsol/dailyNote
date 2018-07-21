d52b453794d7e2ffed4ab2ea5dc24b2caa69e429
전솔 <jeonsol12@naver.com>
2018-07-21 12:09:09 +0900

    메뉴슬라이드 수동으로 구현

--- DailyNote/ViewController.swift
+++ DailyNote/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leadingC: NSLayoutConstraint!
+    @IBOutlet weak var trailingC: NSLayoutConstraint!
+    @IBOutlet weak var ubeView: UIView!
+    
+    var hamverMenuIsVisible: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if !hamverMenuIsVisible {
+            leadingC.constant = 450
+            trailingC.constant = -450
+            
+            hamverMenuIsVisible = true
+        }
+        
+        else {
+            leadingC.constant = 0
+            trailingC.constant = 0
+            
+            hamverMenuIsVisible = false
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0.2, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
 }
 
