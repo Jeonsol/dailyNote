2b9b2dbd84b014c3fbd6f83c35f3df98c239d8b7
전솔 <jeonsol12@naver.com>
2018-07-21 13:20:15 +0900

    메뉴슬라이드 SWRevealViewController로 구현

--- DailyNote/ViewController.swift
+++ DailyNote/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
 
     override func didReceiveMemoryWarning() {
